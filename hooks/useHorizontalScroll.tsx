d0c8e86f606a0dca35be147fd1ffa54b2cbb8825
Konstantin <konstantin.muenster@mail.com>
2022-05-16 06:54:41 +0200

    use horizontal scroll only for actual mice

--- hooks/useHorizontalScroll.tsx
+++ hooks/useHorizontalScroll.tsx
@@ -1,5 +1,7 @@
 import { useRef, useEffect } from 'react';
 
+type onWheelHandler = (e: WheelEvent) => void;
+
 export const useHorizontalScroll = () => {
   const ref = useRef<HTMLDivElement>(null);
 
@@ -7,20 +9,31 @@ export const useHorizontalScroll = () => {
     const element = ref.current;
 
     if (element) {
-      const onWheel = (e: WheelEvent) => {
+      const onWheel = (e: WheelEvent & { wheelDeltaY: number }) => {
+        // If the user didn't scroll some pixels, do nothing
+        if (e.deltaY === 0) return;
+
+        // If the user uses a trackpad instead an actual mouse, do nothing
+        const isTrackpad = e.wheelDeltaY
+          ? e.wheelDeltaY === e.deltaY * -3
+          : e.deltaMode === 0;
+        if (isTrackpad) return;
+
+        // If the user reached the start or end of the container, do nothing
         const isAtStartAndUpwards = element.scrollLeft === 0 && e.deltaY <= 0;
         const isAtEndAndDownwards =
           element.offsetWidth + element.scrollLeft >= element.scrollWidth &&
           e.deltaY >= 0;
-
-        if (e.deltaY === 0 || isAtStartAndUpwards || isAtEndAndDownwards)
-          return;
+        if (isAtStartAndUpwards || isAtEndAndDownwards) return;
 
         e.preventDefault();
         element.scrollTo({ left: element.scrollLeft + e.deltaY });
       };
-      element.addEventListener('wheel', onWheel);
-      return () => element.removeEventListener('wheel', onWheel);
+
+      element.addEventListener('wheel', onWheel as onWheelHandler);
+
+      return () =>
+        element.removeEventListener('wheel', onWheel as onWheelHandler);
     }
   }, []);
 
