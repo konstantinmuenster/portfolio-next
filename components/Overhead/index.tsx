b9f67a3e062760b03384bbdadcf0f471070e3d80
Konstantin <konstantin.muenster@mail.com>
2022-05-30 15:14:01 +0200

    add trending topics component

---
+++ components/Overhead/index.tsx
@@ -0,0 +1,9 @@
+import { styled } from '@config/stitches.config';
+
+export const Overhead = styled('span', {
+  fontSize: '$mini',
+  lineHeight: '$mini',
+  fontWeight: 500,
+  textTransform: 'uppercase',
+  color: '$secondary900',
+});
---
+++ components/TrendingTopics/index.tsx
@@ -0,0 +1,115 @@
+import { useMemo } from 'react';
+import { NextRouter, useRouter } from 'next/router';
+import qs from 'query-string';
+
+import { darkTheme, styled } from '@config/stitches.config';
+import { BlogPostMatter } from '@pages/blog/[slug]';
+import { Overhead } from '@components/Overhead';
+
+const StyledTrendingTopics = styled('div', {
+  '.trending-topics-list': {
+    my: '1rem',
+    display: 'flex',
+    flexDirection: 'row',
+    columnGap: '0.5rem',
+
+    button: {
+      py: '0.25rem',
+      px: '0.375rem',
+      fontSize: '$mini',
+      borderRadius: '$less',
+
+      color: '$subtext',
+      background: '$turquoise',
+      transition: '$default',
+
+      [`.${darkTheme} &`]: { backgroundColor: '$secondary100' },
+
+      '&:hover, &[data-selected="true"]': {
+        color: '$primary900',
+        background: '$primary50',
+      },
+    },
+  },
+});
+
+type TrendingTopicsProps = {
+  posts: BlogPostMatter[];
+};
+
+export const TrendingTopics: React.FC<TrendingTopicsProps> = props => {
+  const router = useRouter();
+  const currentTags = getTagsFromQuery(router);
+
+  const trendingTopics = useMemo(
+    () => getTrendingTopics(props.posts, { count: 5 }),
+    [props.posts]
+  );
+
+  const updateTagsQuery = (tags: string[]) => {
+    const query = qs.stringify({ tags }, { arrayFormat: 'comma' });
+    router.push(`${router.pathname}?${query}`);
+  };
+
+  const addSelectedTag = (tag: string) => {
+    updateTagsQuery([...currentTags, tag]);
+    document.querySelector('footer')?.scrollIntoView();
+  };
+
+  const removeSelectedTag = (tag: string) => {
+    updateTagsQuery(currentTags.filter(v => v !== tag));
+  };
+
+  const handleClickOnTag = (tag: string) => {
+    if (currentTags.includes(tag)) removeSelectedTag(tag);
+    else addSelectedTag(tag);
+  };
+
+  return (
+    <StyledTrendingTopics>
+      <Overhead>Trending Topics</Overhead>
+      <div className="trending-topics-list">
+        {trendingTopics.map(({ name }, key) => {
+          return (
+            <button
+              key={key}
+              onClick={() => handleClickOnTag(name)}
+              data-selected={currentTags.includes(name)}
+              aria-label="Filter Articles By Topic"
+            >
+              {name}
+            </button>
+          );
+        })}
+      </div>
+    </StyledTrendingTopics>
+  );
+};
+
+const getTrendingTopics = (
+  posts: BlogPostMatter[],
+  { count }: { count?: number } = {}
+) => {
+  const tags = posts.reduce((tags, post) => {
+    const newTags = (post.tags ?? []).reduce((_, tag) => {
+      const tagKey = tag.toLowerCase();
+
+      if (tags.hasOwnProperty(tagKey)) tags[tagKey].count++;
+      else tags[tagKey] = { name: tag, count: 1 };
+
+      return tags;
+    }, {} as Record<string, { name: string; count: number }>);
+
+    return { ...tags, ...newTags };
+  }, {} as Record<string, { name: string; count: number }>);
+
+  return Object.values(tags)
+    .sort(({ count: ac }, { count: bc }) => bc - ac)
+    .slice(0, count ?? 5);
+};
+
+const getTagsFromQuery = (router: NextRouter) => {
+  if (!router.query.tags) return [];
+  if (Array.isArray(router.query.tags)) return router.query.tags;
+  return router.query.tags.split(',');
+};
