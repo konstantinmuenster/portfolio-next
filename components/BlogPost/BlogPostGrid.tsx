1f49e7fdee76a24ea3e892d044c8a36b4ca9db46
Konstantin <konstantin.muenster@mail.com>
2022-05-30 18:58:52 +0200

    fix hydration error

--- components/BlogPost/BlogPostGrid.tsx
+++ components/BlogPost/BlogPostGrid.tsx
@@ -1,5 +1,7 @@
-import { BlogPostMatter } from '@pages/blog/[slug]';
 import { Masonry } from 'masonic';
+
+import type { BlogPostMatter } from '@pages/blog/[slug]';
+import { useHasMounted } from '@hooks/useHasMounted';
 import { BlogPostGridItem } from './BlogPostGridItem';
 
 type BlogPostGridProps = {
@@ -7,6 +9,10 @@ type BlogPostGridProps = {
 };
 
 export const BlogPostGrid: React.FC<BlogPostGridProps> = props => {
+  const mounted = useHasMounted();
+
+  if (!mounted) return <></>;
+
   return (
     <Masonry
       items={props.posts}
