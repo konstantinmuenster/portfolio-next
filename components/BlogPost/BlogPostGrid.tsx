404cf64170ed282baaef7443159d6e1a5a19f5a2
Konstantin <konstantin.muenster@mail.com>
2022-06-02 17:13:04 +0200

    add placeholder for no articles in blog grid

--- components/BlogPost/BlogPostGrid.tsx
+++ components/BlogPost/BlogPostGrid.tsx
@@ -3,6 +3,15 @@ import { Masonry } from 'masonic';
 import type { BlogPostMatter } from '@pages/blog/[slug]';
 import { useHasMounted } from '@hooks/useHasMounted';
 import { BlogPostGridItem } from './BlogPostGridItem';
+import { styled } from '@config/stitches.config';
+
+const Styled404Placeholder = styled('div', {
+  py: '2rem',
+  color: '$subtext',
+  lineHeight: '$big',
+  fontSize: '$big',
+  fontWeight: 500,
+});
 
 type BlogPostGridProps = {
   posts: BlogPostMatter[];
@@ -13,8 +22,12 @@ export const BlogPostGrid: React.FC<BlogPostGridProps> = props => {
 
   if (!mounted) return <></>;
 
+  if (!props.posts.length)
+    return <Styled404Placeholder>No Articles found 😥</Styled404Placeholder>;
+
   return (
     <Masonry
+      key={props.posts.length}
       items={props.posts}
       render={BlogPostGridItem}
       columnWidth={380}
