d50e6a3b300cf3a62921c68fcc9d7fac3f05d829
Konstantin <konstantin.muenster@mail.com>
2022-06-27 13:22:11 +0200

    simplify getBaseUrl

--- utils/getBaseUrl.ts
+++ utils/getBaseUrl.ts
@@ -1,4 +1,2 @@
 export const getBaseUrl = () =>
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : process.env.SITE_URL ?? 'https://konstantin.digital';
+  process.env.SITE_URL ?? 'https://konstantin.digital';
