0db36353a1e5239e5b25c5908a0785ea605ace60
Konstantin <konstantin.muenster@mail.com>
2022-07-03 07:35:18 +0200

    add animation to dropdown menu

--- components/DropdownMenu/index.tsx
+++ components/DropdownMenu/index.tsx
@@ -1,4 +1,4 @@
-import { Menu } from '@headlessui/react';
+import { Menu, Transition } from '@headlessui/react';
 
 import { NavigationItem } from '@config/navigation.config';
 import { styled } from '@config/stitches.config';
@@ -9,6 +9,13 @@ import { HamburgerButton } from './HamburgerButton';
 
 const StyledDropdownMenu = styled('nav', {
   position: 'relative',
+
+  '.enter-animation': { transition: 'all ease-in 100ms' },
+  '.exit-animation': { transition: 'all ease-out 75ms' },
+  '.opacity-animation-start': { opacity: 0 },
+  '.opacity-animation-end': { opacity: 1 },
+  '.scale-animation-start': { transform: 'scale(0.95)' },
+  '.scale-animation-end': { transform: 'scale(1)' },
 });
 
 type DropdownMenuProps = {
@@ -25,7 +32,16 @@ export const DropdownMenu: React.FC<DropdownMenuProps> = props => {
         {({ open }) => (
           <>
             <HamburgerButton open={open} />
-            <MenuItems items={props.items} />
+            <Transition
+              enter="enter-animation"
+              enterFrom="opacity-animation-start scale-animation-start"
+              enterTo="opacity-animation-end scale-animation-end"
+              leave="exit-animation"
+              leaveFrom="opacity-animation-end scale-animation-end"
+              leaveTo="opacity-animation-start scale-animation-start"
+            >
+              <MenuItems items={props.items} />
+            </Transition>
           </>
         )}
       </Menu>
