d6a16fa07bab6e75dbf58860e6cd802d2d3349eb
Konstantin <konstantin.muenster@mail.com>
2022-07-09 07:26:16 +0200

    redesign contact card

--- components/Card/Contact.tsx
+++ components/Card/Contact.tsx
@@ -1,31 +1,34 @@
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 
+import { styled } from '@config/stitches.config';
+import { socialProfiles } from '@config/profiles.config';
 import { ContentWrapper } from '@components/Layout';
 import { SparkSvg } from '@components/TextDecoration/SparkSvg';
 import { Avatar } from '@components/Avatar';
-import { styled } from '@config/stitches.config';
-import { socialProfiles } from '@config/profiles.config';
+import { Link } from '@components/Link';
 import { getEmailAddress } from '@utils/getEmailAddress';
+import { getCurrentTimestamp } from '@utils/date/getCurrentTimestamp';
 
 const StyledContactCard = styled('div', {
-  '> div': {
+  marginBottom: '6rem',
+
+  '.contact-card-wrapper': {
     position: 'relative',
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'flex-start',
+    alignItems: 'flex-end',
     justifyContent: 'flex-start',
     rowGap: '2rem',
     py: '2rem',
     px: '1rem',
     background: '$secondary50',
     borderRadius: '$default',
-    marginBottom: '2rem',
 
     '@md': {
       flexDirection: 'row',
       alignItems: 'center',
       justifyContent: 'space-between',
-      columnGap: '2rem',
+      columnGap: '4rem',
       rowGap: 'unset',
       px: '1.5rem',
     },
@@ -40,93 +43,94 @@ const StyledContactCard = styled('div', {
     },
   },
 
-  '.contact-me': {
-    display: 'flex',
-    columnGap: '0.5rem',
-    background: '$primary50',
-    paddingLeft: '1rem',
-    paddingRight: '2rem',
-    py: '0.75rem',
-    borderRadius: '$default',
+  '.contact-card-content': {
+    h5: { marginBottom: '0.5rem' },
+  },
 
-    '.contact-name': {
-      display: 'block',
-      fontWeight: 500,
-      color: '$primary900',
+  '.contact-card-chat': {
+    width: '100%',
+    maxWidth: '240px',
+
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    gap: '0.5rem',
+
+    '.contact-card-chat-message': {
+      width: '100%',
+      position: 'relative',
+      display: 'flex',
+      alignItems: 'center',
+      columnGap: '0.5rem',
+      background: '$secondary100',
+      borderRadius: '$default',
+      padding: '0.5rem',
+
+      p: { fontSize: '$small', lineHeight: '$small' },
+
+      '.timestamp': {
+        position: 'absolute',
+        bottom: 6,
+        right: 8,
+        fontSize: '$mini',
+        lineHeight: '$mini',
+        color: '$subtext',
+      },
     },
 
-    '.contact-email': {
-      button: {
-        fontSize: '$mini',
-        fontWeight: 500,
-        color: '$secondary900',
-        transition: '$default',
-        width: '100%',
+    '.contact-card-email': {
+      display: 'inline-block',
+      py: '0.5rem',
+      px: '1rem',
+      background: '$primary250',
+      transition: '$default',
+      color: '$background',
+      borderRadius: '$less',
 
-        '&:hover': { color: '$secondary500' },
-      },
+      '&:hover': { background: '$pinky' },
     },
   },
 });
 
 export const ContactCard: React.FC = () => {
-  const [isHoveringCopy, setIsHoveringCopy] = useState(false);
-  const [isCopied, setIsCopied] = useState(false);
-
   const email = useMemo(() => {
-    return getEmailAddress(socialProfiles);
+    return socialProfiles.find(({ to }) => to.includes('mailto:'));
   }, []);
 
-  const handleCopyClick = async () => {
-    await navigator.clipboard.writeText(email ?? '');
-    setIsCopied(true);
-    setTimeout(() => {
-      setIsCopied(false);
-    }, 1000);
-  };
-
-  const setHoveringState = (value: boolean) => {
-    setTimeout(() => {
-      if (value === true && !isHoveringCopy) {
-        setIsHoveringCopy(true);
-      } else if (value === false && isHoveringCopy) {
-        setIsHoveringCopy(false);
-      }
-    }, 100);
-  };
-
-  const buttonLabel = isCopied
-    ? 'Copied!'
-    : isHoveringCopy
-    ? 'Copy Email Address'
-    : email;
+  const linkedIn = useMemo(() => {
+    return socialProfiles.find(({ label }) =>
+      label.toLowerCase().includes('linkedin')
+    );
+  }, []);
 
   return (
     <StyledContactCard>
       <ContentWrapper>
-        <SparkSvg aria-hidden={true} data-text-decoration />
-        <div className="need-help">
-          <h5>Need help or advice?</h5>
-          <p>
-            Or just want to have a nice chat? Either way, feel free to contact
-            me!
-          </p>
-        </div>
-        <div className="contact-me">
-          <Avatar size={40} />
-          <div className="contact-details">
-            <span className="contact-name">Konstantin Münster</span>
-            <div className="contact-email">
-              <button
-                onClick={handleCopyClick}
-                onMouseEnter={() => setHoveringState(true)}
-                onMouseLeave={() => setHoveringState(false)}
-                disabled={isCopied}
-                aria-label={buttonLabel}
-              >
-                {buttonLabel}
-              </button>
+        <div className="contact-card-wrapper">
+          <SparkSvg aria-hidden={true} data-text-decoration />
+          <div className="contact-card-content">
+            <h5>Need help or advice?</h5>
+            <p>
+              Or just want to have a nice chat? Either way, feel free to contact
+              me! You can reach me best by email or on{' '}
+              <Link to={linkedIn?.to ?? ''}>LinkedIn</Link>.
+            </p>
+          </div>
+          <div className="contact-card-chat">
+            <div className="contact-card-chat-message">
+              <Avatar size={24} />
+              <p>Looking forward to it!</p>
+              <span className="timestamp">{getCurrentTimestamp()}</span>
             </div>
+            <Link
+              to={email?.to ?? ''}
+              role="button"
+              aria-label="Write an email"
+              className="contact-card-email"
+              hideExternalHint
+            >
+              {getEmailAddress(socialProfiles)}
+            </Link>
           </div>
         </div>
       </ContentWrapper>
